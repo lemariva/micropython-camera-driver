aedf6580c45c66e0782fe6fdb4d5e39dfb4a66fb
Mauro Riva <lemariva@gmail.com>
2020-06-20 23:36:06 +0200

    (init) initial commit

--- README.md
+++ README.md
@@ -1,1 +1,98 @@
 # micropython-camera-driver
+
+This repository adds camera (OV2640) support to MicroPython for the ESP32 family. 
+
+I could have forked the micropython repository and include the camera driver. However, I chose to include in this repository only the files that are required, so that you can always use the last version of MicroPython and add these files to add camera support.
+
+Furthermore, I include a compiled version of the firmware, so that you can flash it and start playing with the camera.
+
+## Example
+```python
+import camera
+
+# ESP32-CAM (default configuration) - https://bit.ly/2Ndn8tN
+camera.init(0, format=camera.JPEG)  
+
+# M5Camera (Version B) - https://bit.ly/317Xb74
+camera.init(0, format=camera.JPEG, d0=32, d1=35, d2=34, d3=5, d4=39, d5=18, d6=36, d7=19, 
+            href=26, vsync=25, reset=15, sioc=23, siod=22, xclk=27, pclk=21)   #M5CAMERA
+
+buf = camera.capture()
+```
+### Important
+* Except when using CIF or lower resolution with JPEG, the driver requires PSRAM to be installed and activated. This is activated, but it is limited due that MicroPython needs RAM.
+* Using YUV or RGB puts a lot of strain on the chip because writing to PSRAM is not particularly fast. The result is that image data might be missing. This is particularly true if WiFi is enabled. If you need RGB data, it is recommended that JPEG is captured and then turned into RGB using `fmt2rgb888 or fmt2bmp/frame2bmp`. The conversion is not supported. The formats are included, but I got almost every time out of memory, trying to capture an image in a different format than JPEG.
+
+## DIY
+To include the camera support to MicroPython, you need to compile the firmware from scratch. To do that follow these steps:
+1. Clone the MicroPython repository:
+    ```
+    git clone --recursive https://github.com/micropython/micropython.git
+    ```
+    Note: The MicroPython repo changes a lot, I've done this using the version with the hash 3a9d948032e27f690e1fb09084c36bd47b1a75a0.
+2. Copy the files of this repository inside the folder `ports/esp32`. If you don't want to replace the files `mpconfigport.h`, `main.h` and `Makefile`, make the following modifications to the originals:
+    * `mpconfigport.c`
+        1. add the line
+        ```
+           extern const struct _mp_obj_module_t mp_module_camera;
+        ```
+        in the code block below `// extra built in modules to add to the list of known ones` (approx. line 184)
+        1. add the line:
+        ```
+            { MP_OBJ_NEW_QSTR(MP_QSTR_camera), (mp_obj_t)&mp_module_camera }, \
+        ```
+        in the code block below `#define MICROPY_PORT_BUILTIN_MODULES \` (approx. line 194/195)
+
+    * `main.c`: modify the lines inside the `#if CONFIG_ESP32_SPIRAM_SUPPORT || CONFIG_SPIRAM_SUPPORT`, they should look like:
+    ```
+        mp_task_heap_size = 2 * 1024 * 1024;
+        void *mp_task_heap = malloc(mp_task_heap_size);
+        ESP_LOGI("main", "Allocated %dK for micropython heap at %p", mp_task_heap_size/1024, mp_task_heap);
+    ```
+
+    * `Makefile`:
+        1. add the lines:
+        ```
+            INC_ESPCOMP += -I$(ESPCOMP)/esp32-camera/driver/include
+            INC_ESPCOMP += -I$(ESPCOMP)/esp32-camera/driver/private_include
+            INC_ESPCOMP += -I$(ESPCOMP)/esp32-camera/conversions/include
+            INC_ESPCOMP += -I$(ESPCOMP)/esp32-camera/conversions/private_include
+            INC_ESPCOMP += -I$(ESPCOMP)/esp32-camera/sensors/private_include
+        ```
+        inside under the `# Compiler and linker flags`  (approx. line 135)
+
+        2. add the line:
+        ```
+            modcamera.c \
+        ```
+        inside the `SRC_C = \` block (approx. line 329 - after the above insertion)
+        3. add the lines:
+        ```
+            ESP32_CAM_O = $(patsubst %.c,%.o,\
+            $(wildcard $(ESPCOMP)/esp32-camera/driver/*.c) \
+            $(wildcard $(ESPCOMP)/esp32-camera/sensors/*.c) \
+            $(wildcard $(ESPCOMP)/esp32-camera/conversions/*.c) \
+            )
+        ```
+        above the line `OBJ_ESPIDF =` (approx. line 615 - after the above insertions)
+        4. add the line:
+        ```
+            $(eval $(call gen_espidf_lib_rule,esp32_cam,$(ESP32_CAM_O)))
+        ```
+        in the block above `ifeq ($(ESPIDF_CURHASH),$(ESPIDF_SUPHASH_V4))` (approx. line 655 - after the above insertions)
+
+3. Compile and deploy MicroPython following the instructions from this [tutorial](https://lemariva.com/blog/2020/03/tutorial-getting-started-micropython-v20). But, use the following compiling options:
+```sh
+make BOARD=GENERIC_CAM PYTHON=python3 MICROPY_PY_BTREE=0 -j
+make BOARD=GENERIC_CAM PYTHON=python3 MICROPY_PY_BTREE=0 -j deploy
+```
+
+## Firmware
+I've included a compiled MicroPython firmware with camera and BLE support (check the `firmware` folder). The firmware was compiled using esp-idf 4.x (hash 4c81978a3).
+
+To deploy it, you need to type the following:
+```sh
+esptool.py --chip esp32 --port /dev/ttyUSB0 erase_flash
+esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 460800 write_flash -z 0x1000 micropython_3a9d948_esp32_idf4.x_ble_camera.bin
+```
+More information is available in this [tutorial](https://lemariva.com/blog/2020/03/tutorial-getting-started-micropython-v20).
---
+++ modcamera.c
@@ -0,0 +1,174 @@
+// Code adapted from https://github.com/espressif/esp32-camera
+
+#include "esp_camera.h"
+#include "esp_log.h"
+
+#include <string.h>
+#include "py/nlr.h"
+#include "py/obj.h"
+#include "modcamera.h"
+#include "py/runtime.h"
+#include "py/binary.h"
+
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+typedef struct _camera_obj_t {
+    int8_t                 id;
+    camera_config_t        config;
+    bool                   used;
+} camera_obj_t;
+
+STATIC camera_obj_t camera_obj;
+        
+
+STATIC void camera_init_helper(camera_obj_t *camera, size_t n_pos_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+      enum {
+        ARG_format,
+        ARG_quality,
+        ARG_d0,
+        ARG_d1,
+        ARG_d2,
+        ARG_d3,
+        ARG_d4,
+        ARG_d5,
+        ARG_d6,
+        ARG_d7,
+        ARG_VSYNC,
+        ARG_HREF,
+        ARG_PCLK,
+        ARG_PWDN,
+        ARG_RESET,
+        ARG_XCLK,
+        ARG_SIOD,
+        ARG_SIOC,
+    };
+
+    //{ MP_QSTR_d0,              MP_ARG_KW_ONLY                   | MP_ARG_OBJ,   {.u_obj = MP_OBJ_NULL} },
+    static const mp_arg_t allowed_args[] = {
+        { MP_QSTR_format,          MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = PIXFORMAT_JPEG} },
+        { MP_QSTR_quality,         MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = 12} },
+        { MP_QSTR_d0,              MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_D0} },
+        { MP_QSTR_d1,              MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_D1} },
+        { MP_QSTR_d2,              MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_D2} },    
+        { MP_QSTR_d3,              MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_D3} },    
+        { MP_QSTR_d4,              MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_D4} },
+        { MP_QSTR_d5,              MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_D5} },   
+        { MP_QSTR_d6,              MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_D6} },    
+        { MP_QSTR_d7,              MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_D7} },   
+        { MP_QSTR_vsync,           MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_VSYNC} },    
+        { MP_QSTR_href,            MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_HREF} },   
+        { MP_QSTR_pclk,            MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_PCLK} },    
+        { MP_QSTR_pwdn,            MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_PWDN} },
+        { MP_QSTR_reset,           MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_RESET} },
+        { MP_QSTR_xclk,            MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_XCLK} },
+        { MP_QSTR_siod,            MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_SIOD} }, 
+        { MP_QSTR_sioc,            MP_ARG_KW_ONLY                   | MP_ARG_INT,   {.u_int = CAM_PIN_SIOC} },
+    };
+
+    mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
+    mp_arg_parse_all(n_pos_args, pos_args, kw_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
+    
+    // TODO:---- Check validity of arguments ----
+    int8_t format = args[ARG_format].u_int;
+    if ((format != PIXFORMAT_JPEG) &&
+        (format != PIXFORMAT_YUV422) &&
+        (format != PIXFORMAT_GRAYSCALE) &&
+        (format != PIXFORMAT_RGB565)) {
+        mp_raise_ValueError(MP_ERROR_TEXT("Image format is not valid"));
+    }
+
+    // configuring camera
+    camera->config.pin_d0 = args[ARG_d0].u_int;
+    camera->config.pin_d1 = args[ARG_d1].u_int;
+    camera->config.pin_d2 = args[ARG_d2].u_int;
+    camera->config.pin_d3 = args[ARG_d3].u_int;
+    camera->config.pin_d4 = args[ARG_d4].u_int;
+    camera->config.pin_d5 = args[ARG_d5].u_int;
+    camera->config.pin_d6 = args[ARG_d6].u_int;
+    camera->config.pin_d7 = args[ARG_d7].u_int;
+    camera->config.pin_vsync = args[ARG_VSYNC].u_int;
+    camera->config.pin_href = args[ARG_HREF].u_int;
+    camera->config.pin_pclk = args[ARG_PCLK].u_int;
+    camera->config.pin_pwdn = args[ARG_PWDN].u_int;
+    camera->config.pin_reset = args[ARG_RESET].u_int;
+    camera->config.pin_xclk = args[ARG_XCLK].u_int;
+    camera->config.pin_sscb_sda = args[ARG_SIOD].u_int;
+    camera->config.pin_sscb_scl = args[ARG_SIOC].u_int;
+    camera->config.pixel_format = args[ARG_format].u_int;   //YUV422,GRAYSCALE,RGB565,JPEG
+    camera->config.jpeg_quality = args[ARG_quality].u_int;  //0-63 lower number means higher quality
+
+    // defaul parameters
+        //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+    camera->config.xclk_freq_hz = 20000000;
+    camera->config.ledc_timer = LEDC_TIMER_0;
+    camera->config.ledc_channel = LEDC_CHANNEL_0;
+    camera->config.frame_size = FRAMESIZE_UXGA;//QQVGA-QXGA Do not use sizes above QVGA when not JPEG
+    camera->config.fb_count = 1; //if more than one, i2s runs in continuous mode. Use only with JPEG
+
+    esp_err_t err = esp_camera_init(&camera->config);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Camera Init Failed");
+        mp_raise_msg(&mp_type_OSError, MP_ERROR_TEXT("Camera Init Failed"));
+    }
+}
+
+
+STATIC mp_obj_t camera_init(mp_uint_t n_pos_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+
+    camera_init_helper(&camera_obj, n_pos_args - 1, pos_args + 1, kw_args);
+    
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_KW(camera_init_obj, 1, camera_init);
+
+
+STATIC mp_obj_t camera_deinit(){
+    esp_err_t err = esp_camera_deinit();
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Camera deinit Failed");
+        return mp_const_false;
+    }
+
+    return mp_const_true;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(camera_deinit_obj, camera_deinit);
+
+
+STATIC mp_obj_t camera_capture(){
+    //acquire a frame
+    camera_fb_t * fb = esp_camera_fb_get();
+    if (!fb) {
+        ESP_LOGE(TAG, "Camera Capture Failed");
+        return mp_const_false;
+    }
+
+    mp_obj_t image = mp_obj_new_bytes(fb->buf, fb->len);
+
+    //return the frame buffer back to the driver for reuse
+    esp_camera_fb_return(fb);
+    return image;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(camera_capture_obj, camera_capture);
+
+
+STATIC const mp_rom_map_elem_t camera_module_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_camera) },
+
+    { MP_ROM_QSTR(MP_QSTR_init), MP_ROM_PTR(&camera_init_obj) },
+    { MP_ROM_QSTR(MP_QSTR_deinit), MP_ROM_PTR(&camera_deinit_obj) },
+    { MP_ROM_QSTR(MP_QSTR_capture), MP_ROM_PTR(&camera_capture_obj) },
+
+    // Constants
+    { MP_ROM_QSTR(MP_QSTR_JPEG),            MP_ROM_INT(PIXFORMAT_JPEG) },
+    { MP_ROM_QSTR(MP_QSTR_YUV422),          MP_ROM_INT(PIXFORMAT_YUV422) },
+    { MP_ROM_QSTR(MP_QSTR_GRAYSCALE),       MP_ROM_INT(PIXFORMAT_GRAYSCALE) },
+    { MP_ROM_QSTR(MP_QSTR_RGB565),          MP_ROM_INT(PIXFORMAT_RGB565) },
+};
+
+STATIC MP_DEFINE_CONST_DICT(camera_module_globals, camera_module_globals_table);
+
+const mp_obj_module_t mp_module_camera = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t *)&camera_module_globals,
+};
