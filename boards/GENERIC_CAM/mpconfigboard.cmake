cb63c0dbbf96ec68549d577204fde99864f12dc9
lorenz <lorenz.roman.gruber@gmail.com>
2021-03-31 12:39:34 +0200

    updated the instructions and firmware to work with cmake build system

---
+++ boards/GENERIC_CAM/mpconfigboard.cmake
@@ -0,0 +1,7 @@
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    boards/sdkconfig.ble
+    boards/sdkconfig.esp32cam
+)
+
+set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/manifest.py)
--- create_tgz.sh
+++ create_tgz.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tar -zcvf micropython-camera-driver-overlay.tgz Makefile mpconfigport.h main.c modcamera.[ch] boards/
+tar -zcvf micropython-camera-driver-overlay.tgz  modcamera.[ch] boards/
